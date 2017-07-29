c8c0a6801bdaa4a8b186ed1383f0b4680d14aa79
Gerald <gera2ld@163.com>
2017-07-30 01:10:10 +0800

    fix: use chrome-style options UI

--- src/options/components/app.vue
+++ src/options/components/app.vue
@@ -1,19 +1,17 @@
 <template>
   <div id="app" class="flex flex-col h-100">
-    <header class="flex flex-center">
-      <img src="/public/images/icon48.png">
-      <h2 class="flex-auto">Request Blocker</h2>
-      <div>&copy; Gerald</div>
-    </header>
     <div class="flex flex-auto">
       <rule-body class="flex-auto"></rule-body>
       <rule-nav></rule-nav>
     </div>
-    <footer>
-      Links:
-      <a href="https://github.com/gera2ld/request-blocker" target="_blank">GitHub</a>
-      |
-      <a href="https://github.com/gera2ld/request-blocker/issues" target="_blank">Feedback</a>
+    <footer class="flex">
+      <div class="flex-auto">&copy; Gerald</div>
+      <div>
+        Links:
+        <a href="https://github.com/gera2ld/request-blocker" target="_blank">GitHub</a>
+        |
+        <a href="https://github.com/gera2ld/request-blocker/issues" target="_blank">Feedback</a>
+      </div>
     </footer>
   </div>
 </template>
@@ -35,9 +33,6 @@ export default {
   max-width: 60rem;
   max-height: 50rem;
   margin: 0 auto;
-  > .flex-auto {
-    margin: 1rem 0;
-  }
   > header {
     > img {
       margin-right: 1rem;
@@ -45,7 +40,6 @@ export default {
   }
   > footer {
     margin-top: .5rem;
-    text-align: center;
   }
 }
 </style>
