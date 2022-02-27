fc8c5d69487472852b9a1c2566e34a6d15f86530
Gerald <gera2ld@live.com>
2022-02-28 00:22:56 +0800

    fix: allow copy text selections

--- src/common/keyboard.ts
+++ src/common/keyboard.ts
@@ -8,25 +8,42 @@ export const keyboardService = new KeyboardService();
 
 bindKeys();
 
+let hasSelection = false;
+let hasInput = false;
+
 export function isInput(el: Element) {
   return ['input', 'textarea'].includes(el?.tagName?.toLowerCase());
 }
 
 function handleFocus(e: FocusEvent) {
   if (isInput(e.target as Element)) {
-    keyboardService.setContext('inputFocus', true);
+    hasInput = true;
+    updateInputFocus();
   }
 }
 
 function handleBlur(e: FocusEvent) {
   if (isInput(e.target as Element)) {
-    keyboardService.setContext('inputFocus', false);
+    hasInput = false;
+    updateInputFocus();
   }
+}
+
+function updateInputFocus() {
+  keyboardService.setContext('inputFocus', hasSelection || hasInput);
 }
 
 function bindKeys() {
   document.addEventListener('focus', handleFocus, true);
   document.addEventListener('blur', handleBlur, true);
+  document.addEventListener(
+    'selectionchange',
+    () => {
+      hasSelection = document.getSelection().type === 'Range';
+      updateInputFocus();
+    },
+    false
+  );
   keyboardService.register(
     'enter',
     () => {
