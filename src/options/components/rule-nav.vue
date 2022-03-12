c240c1f4e0b260467c15b724c9cedd35347568ac
Gerald <gera2ld@live.com>
2022-03-12 17:41:04 +0800

    fix: tweak styles

--- src/options/components/rule-nav.vue
+++ src/options/components/rule-nav.vue
@@ -8,28 +8,26 @@
       <input type="search" v-model="filter" placeholder="Filter by name" />
     </div>
     <div class="flex-1 overflow-y-auto">
-      <section class="nav-section">
-        <ListSection
-          type="request"
-          :lists="store.lists.request"
-          :index="0"
-          :filter="filter"
-        >
-          <template #title>Request Interception</template>
-        </ListSection>
-        <ListSection
-          type="cookie"
-          :lists="store.lists.cookie"
-          :index="1"
-          :filter="filter"
-          :unsupported="!store.features.cookies"
-        >
-          <template #title>Cookie Interception</template>
-          <template #unsupported>
-            <div class="subtle">Not supported in this browser.</div>
-          </template>
-        </ListSection>
-      </section>
+      <ListSection
+        type="request"
+        :lists="store.lists.request"
+        :index="0"
+        :filter="filter"
+      >
+        <template #title>Request Interception</template>
+      </ListSection>
+      <ListSection
+        type="cookie"
+        :lists="store.lists.cookie"
+        :index="1"
+        :filter="filter"
+        :unsupported="!store.features.cookies"
+      >
+        <template #title>Cookie Interception</template>
+        <template #unsupported>
+          <div class="subtle">Not supported in this browser.</div>
+        </template>
+      </ListSection>
     </div>
   </div>
 </template>
